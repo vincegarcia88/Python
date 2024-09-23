f5123e9b8ee909eaa1c2cb04a15262efe62c87e0
Vince Garcia <126945619+vincegarcia88@users.noreply.github.com>
2024-09-23 12:27:22 +0100

    Add files via upload

---
+++ Funções/Funcões.py
@@ -0,0 +1,35 @@
+
+# Hora e data atual
+datetime.now()
+
+#Somente o ano atual
+data.year
+
+#Função reversed
+
+#Função enumerate
+
+#Função sorted
+
+#Diminuir casas decimais em float
+:.2f
+
+# Retorna valores mesmo se for em int ou float
+type()
+
+
+#Arredonda (função)
+round()
+
+# Insere uma linha
+insere_linha ()
+
+def nome_da_funçao ():
+    print("Lalalalala")
+
+#def pede que deixe 2 linhas em branco antes e dps
+
+# imports
+# def
+# codigo principal
+
